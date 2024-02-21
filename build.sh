5569c7af3072e19a8cba650b7d110955c5a07cda
Wouter van de Molengraft <wouter@softmedia.nl>
2024-02-21 09:02:49 +0100

    updated to node 20.11.1 and ffmpeg 5.1.4

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ ! -e bin ]; then
-    mkdir bin
-fi
-
-docker build -t vaem/ffmpeg-build -f Dockerfile.build .
-docker run --rm -it -v "$PWD:/mount" vaem/ffmpeg-build cp /usr/bin/{ffmpeg,ffprobe} /mount/bin
-docker build -t vaem/node-ffmpeg -f Dockerfile .
